e32e04ed6900c017c94d2c62d20617c391748809
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-03-01 22:29:26 +0100

    Add simulation

--- simulate_circle_graph.py
+++ simulate_circle_graph.py
@@ -5,26 +5,35 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+from generate_graph import generate
 
 now = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
-os.mkdir(now)
+
+def save_sparse_csr(filename, array):
+    np.savez(filename, data=array.data, indices=array.indices,
+             indptr=array.indptr, shape=array.shape)
+
+def load_sparse_csr(filename):
+    loader = np.load(filename)
+    return csr_matrix((loader['data'], loader['indices'], loader['indptr']),
+                      shape=loader['shape'])
 
 def move(pos, N, graph):
-  pos = np.random.choice(N, 1, p=graph[pos,:].flatten())
+  pos = np.random.choice(N, 1, p=graph[pos,:].todense().flat)
   return pos
 
-def simulate(N,simulations,steps,connections,random_factor):
-  graph = generate_graph(N,connections,random_factor)
+def simulate(N,simulations,steps,w,density):
+  graph = generate(N,w,density)
   start = N // 2
 
-  print(f"N={N}, start={start}, simulations={simulations}, steps={steps}")
+  print(f"N={N}, w={w}, density={density}, start={start}, simulations={simulations}, steps={steps}")
 
   counts = np.zeros([steps+1, N])
   counts += 0.01
   for _ in progressbar.progressbar(range(simulations)):
     pos = start
     counts[0,pos] += 1
-    for step_i in range(1,steps+1): 
+    for step_i in range(1,steps+1):
       pos = move(pos, N, graph)
       counts[step_i,pos] += 1
 
@@ -40,16 +49,18 @@ def simulate(N,simulations,steps,connections,random_factor):
       shading='auto',
       norm=colors.LogNorm(vmin=counts.min(), vmax=counts.max())
   )
-  ax.set_title(f'Csúcsok száma: {N}\nKiindulási csúcs: {start}\nFutások darabszáma: {simulations}\nLépések száma egy futásban: {steps}\n', loc='left')
+  ax.set_title(f'Csúcsok száma: {N}\nSzélesség: {w}\nVéletlen élek: {density}\nKiindulási csúcs: {start}\nFutások darabszáma: {simulations}\nLépések száma egy futásban: {steps}\n', loc='left')
   ax.set_xlabel('Csúcsindexek')
   ax.set_ylabel('Lépések')
   fig.colorbar(pcm, ax=ax, extend='max')
+  filename = f'{now}_counts_circle_N_{N}_w_{w}_density_{density}_start_{start}_simulations_{simulations}_steps_{steps}'
   fig.savefig(
-      f'{now}/counts_circle_N_{N}_start_{start}_simulations_{simulations}_steps_{steps}.png',
+      f'{filename}.png',
       dpi=600,
       bbox_inches='tight'
   )
   plt.close(fig)
+  save_sparse_csr(f'{filename}_graph.txt', graph)
 
 if __name__ == '__main__':
     # Csúcsok száma, futások száma, lépések száma 
