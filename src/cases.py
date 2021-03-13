1f4d8d0d876592d27ad59bf599fb4db2247dae28
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-03-13 13:19:27 +0100

    Use python formatter, refactor

---
+++ src/cases.py
@@ -0,0 +1,16 @@
+from generate_graph import simulate
+
+simulate(101, 1, 1000, 1, 0)
+simulate(101, 1, 1000, 2, 0)
+simulate(101, 1, 1000, 3, 0)
+simulate(101, 1, 1000, 3, 0.2)
+
+simulate(101, 10, 1000, 1, 0)
+simulate(101, 10, 1000, 2, 0)
+simulate(101, 10, 1000, 3, 0)
+simulate(101, 10, 1000, 3, 0.2)
+
+simulate(101, 1000, 1000, 1, 0)
+simulate(101, 1000, 1000, 2, 0)
+simulate(101, 1000, 1000, 3, 0)
+simulate(101, 1000, 1000, 3, 0.2)
--- src/simulate_circle_graph.py
+++ src/simulate_circle_graph.py
@@ -9,86 +9,71 @@ from generate_graph import generate
 
 file_location = "../generations/new"
 
+
 def get_now():
-  return  datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+    return datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+
 
 def move(pos, N, graph):
-  pos = np.random.choice(N, 1, p=graph[pos,:].todense().flat)
-  return pos
+    pos = np.random.choice(N, 1, p=graph[pos, :].todense().flat)
+    return pos
 
-def simulate(N,simulations,steps,w,density):
-  now = get_now()
-  graph = generate(N,w,density)
-  start = N // 2
 
-  print(now)
-  print(
-    f'N={N}, '
-    f'w={w}, '
-    f'density={density}, '
-    f'start={start}, '
-    f'simulations={simulations}, '
-    f'steps={steps}'
+def simulate(N, simulations, steps, w, density):
+    now = get_now()
+    graph = generate(N, w, density)
+    start = N // 2
+
+    print(now)
+    print(
+        f'N={N}, '
+        f'w={w}, '
+        f'density={density}, '
+        f'start={start}, '
+        f'simulations={simulations}, '
+        f'steps={steps}'
     )
 
-  counts = np.zeros([steps+1, N])
-  counts += 0.01
-  for _ in progressbar.progressbar(range(simulations)):
-    pos = start
-    counts[0,pos] += 1
-    for step_i in range(1,steps+1):
-      pos = move(pos, N, graph)
-      counts[step_i,pos] += 1
+    counts = np.zeros([steps+1, N])
+    counts += 0.01
+    for _ in progressbar.progressbar(range(simulations)):
+        pos = start
+        counts[0, pos] += 1
+        for step_i in range(1, steps+1):
+            pos = move(pos, N, graph)
+            counts[step_i, pos] += 1
 
-  steps_Y = np.arange(-0.5, steps, 1)
-  vertexes_X = np.arange(-0.5, N-1, 1)
+    steps_Y = np.arange(-0.5, steps, 1)
+    vertexes_X = np.arange(-0.5, N-1, 1)
 
-  fig, ax = plt.subplots(1,1,figsize=(6,9))
-  pcm = ax.pcolormesh(
-      vertexes_X,
-      steps_Y,
-      counts,
-      cmap='plasma',
-      shading='auto',
-      norm=colors.LogNorm(vmin=counts.min(), vmax=counts.max())
-  )
-  ax.set_title(
-    f'Csúcsok száma: {N}\n'
-    f'Szélesség: {w}\n'
-    f'Véletlen élek: {density}\n'
-    f'Kiindulási csúcs: {start}\n'
-    f'Futások darabszáma: {simulations}\n'
-    f'Lépések száma egy futásban: {steps}',
-    loc='left'
+    fig, ax = plt.subplots(1, 1, figsize=(6, 9))
+    pcm = ax.pcolormesh(
+        vertexes_X,
+        steps_Y,
+        counts,
+        cmap='plasma',
+        shading='auto',
+        norm=colors.LogNorm(vmin=counts.min(), vmax=counts.max())
     )
-  ax.set_xlabel('Csúcsindexek')
-  ax.set_ylabel('Lépések')
-  fig.colorbar(pcm, ax=ax, extend='max')
+    ax.set_title(
+        f'Csúcsok száma: {N}\n'
+        f'Szélesség: {w}\n'
+        f'Véletlen élek: {density}\n'
+        f'Kiindulási csúcs: {start}\n'
+        f'Futások darabszáma: {simulations}\n'
+        f'Lépések száma egy futásban: {steps}',
+        loc='left'
+    )
+    ax.set_xlabel('Csúcsindexek')
+    ax.set_ylabel('Lépések')
+    fig.colorbar(pcm, ax=ax, extend='max')
 
-  filename = f'{file_location}/{now}_counts_circle'
-  fig.savefig(
-      f'{filename}.png',
-      dpi=600,
-      bbox_inches='tight'
-  )
-  plt.close(fig)
-  with open(f'{filename}_graph.txt', 'w') as f:
-    f.write(str(graph)) #TODO print full graph
-
-if __name__ == '__main__':
-    # Csúcsok száma, futások száma, lépések száma, szélesség, density
-    simulate(101, 1, 1000, 1, 0)
-    simulate(101, 1, 1000, 2, 0)
-    simulate(101, 1, 1000, 3, 0)
-    simulate(101, 1, 1000, 3, 0.2)
-
-    simulate(101, 10, 1000, 1, 0)
-    simulate(101, 10, 1000, 2, 0)
-    simulate(101, 10, 1000, 3, 0)
-    simulate(101, 10, 1000, 3, 0.2)
-
-    simulate(101, 1000, 1000, 1, 0)
-    simulate(101, 1000, 1000, 2, 0)
-    simulate(101, 1000, 1000, 3, 0)
-    simulate(101, 1000, 1000, 3, 0.2)
-
+    filename = f'{file_location}/{now}_counts_circle'
+    fig.savefig(
+        f'{filename}.png',
+        dpi=600,
+        bbox_inches='tight'
+    )
+    plt.close(fig)
+    with open(f'{filename}_graph.txt', 'w') as f:
+        f.write(str(graph))  # TODO print full graph
