25bcd27772e46222e6661eb11d4d868fc23ec273
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-03-13 17:23:11 +0100

    Add dumbbell graph

--- src/cases.py
+++ src/cases.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from simulators.classical import simulate_classical, draw_classical
 from generators.circular_graph import generate_circular_graph, draw_circular_graph
+from generators.dumbbell_graph import generate_dumbbell_graph, draw_dumbbell_graph
 import matplotlib.pyplot as plt
 from pathlib import Path
+import sys
 
 plt.rcParams.update({'font.size': 50})
 
@@ -33,8 +35,25 @@ def run_circular_classical_case(vertex_count, walker_count, step_count, connecti
     plt.close(fig)
 
 
-run_circular_classical_case(51, 1, 1000, [1], 0, )
-run_circular_classical_case(51, 1, 1000, [1, 2], 0,)
+def run_dumbbell_classical_case(vertex_count, walker_count, step_count, connection_distances, density):
+    filename = f'{new}/{now()}_dumbbell_classical.jpg'
+    fig, ax = plt.subplots(1, 2, figsize=(
+        60, 45), gridspec_kw={'width_ratios': [3, 1]})
+    graph = generate_dumbbell_graph(
+        vertex_count, connection_distances, density)
+    draw_dumbbell_graph(graph, ax[0])
+    start = vertex_count // 2
+    counts = simulate_classical(graph, start, walker_count, step_count)
+    draw_classical(vertex_count, step_count, counts, ax[1])
+    fig.savefig(filename, dpi=150, bbox_inches='tight')
+    plt.close(fig)
+
+
+run_dumbbell_classical_case(50, 1, 1000, [1, 2], 0)
+sys.exit()
+
+run_circular_classical_case(51, 1, 1000, [1], 0)
+run_circular_classical_case(51, 1, 1000, [1, 2], 0)
 run_circular_classical_case(51, 1, 1000, [1, 2, 3], 0)
 run_circular_classical_case(51, 1, 1000, [1, 2, 3], 0.002)
 
