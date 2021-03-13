0faacac782a9cedc36ee9aa04482dfff141038f3
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-03-13 13:48:28 +0100

    Draw on subplt

--- src/cases.py
+++ src/cases.py
@@ -8,6 +8,9 @@ def now():
     return datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
 
 
+# def run_case(vertex_count, walker_count, num_of_steps, w, density, filename):
+
+
 simulate(101, 1, 1000, 1, 0, f'{root_path}/{now()}_counts_circle')
 simulate(101, 1, 1000, 2, 0, f'{root_path}/{now()}_counts_circle')
 simulate(101, 1, 1000, 3, 0, f'{root_path}/{now()}_counts_circle')
--- src/generate_graph.py
+++ src/generate_graph.py
@@ -5,45 +5,44 @@ from scipy.sparse import rand
 from scipy.sparse import csr_matrix
 from sklearn.preprocessing import normalize
 
+
 def generate(N, distances, density):
-  identity = np.array(range(N))
-  shifted = (identity + 1) % N
-  ones = np.array([1]*N)
+    identity = np.array(range(N))
+    shifted = (identity + 1) % N
+    ones = np.array([1]*N)
 
-  # Initialise loopbacks
-  graph = csr_matrix((ones, (identity, identity)), shape=(N, N))
+    # Initialise loopbacks
+    graph = csr_matrix((ones, (identity, identity)), shape=(N, N))
 
-  # Next neighbour connections forwards and backwards
-  perm_fwd = csr_matrix((ones, (identity, shifted)), shape=(N, N))
-  perm_bck = csr_matrix((ones, (shifted, identity)), shape=(N, N))
+    # Next neighbour connections forwards and backwards
+    perm_fwd = csr_matrix((ones, (identity, shifted)), shape=(N, N))
+    perm_bck = csr_matrix((ones, (shifted, identity)), shape=(N, N))
 
-  add_fwd = perm_fwd
-  add_bck = perm_bck
+    add_fwd = perm_fwd
+    add_bck = perm_bck
 
-  for i in range(1, max(distances)+1):
-    
-    if i in distances:
-      graph += add_fwd
-      graph += add_bck
+    for i in range(1, max(distances)+1):
 
-    add_fwd *= perm_fwd
-    add_bck *= perm_bck
+        if i in distances:
+            graph += add_fwd
+            graph += add_bck
 
-  rand_edges = rand(N, N, density=density, format='csr')
+        add_fwd *= perm_fwd
+        add_bck *= perm_bck
 
-  graph += rand_edges
-  graph.data[:] = 1
+    rand_edges = rand(N, N, density=density, format='csr')
 
-  graph = normalize(graph, norm='l1')
-  return graph
+    graph += rand_edges
+    graph.data[:] = 1
+
+    graph = normalize(graph, norm='l1')
+    return graph
+
 
 if __name__ == '__main__':
-  graph = generate(50,[1,10],0)
-  G = nx.from_scipy_sparse_matrix(graph)
-  pos = nx.circular_layout(G)
-  nx.draw_networkx(G, pos=pos, with_labels=True)
-  plt.draw()
-  plt.show()
-  pos = np.random.choice(10, 1, p=graph[1,:].todense().flat)
-  print(graph)
-  print(pos)
+    graph = generate(50, [1, 10], 0)
+    G = nx.from_scipy_sparse_matrix(graph)
+    pos = nx.circular_layout(G)
+    fig, ax = plt.subplots(1, 1, figsize=(9, 9))
+    nx.draw_networkx(G, pos=pos, with_labels=True, ax=ax)
+    fig.savefig('../generations/new/cucc.png', dpi=600)
