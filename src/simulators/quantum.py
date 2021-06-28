6b125ff2a2c9472a8134ac149fdf869584f9a53f
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-06-28 21:44:54 +0200

    Print states

--- src/simulators/quantum.py
+++ src/simulators/quantum.py
@@ -5,6 +5,10 @@ import progressbar
 from scipy.linalg import hadamard
 
 
+np.set_printoptions(threshold=np.inf)
+np.set_printoptions(linewidth=np.inf)
+
+
 class Quantum():
 
   # http://susan-stepney.blogspot.com/2014/02/mathjax.html
@@ -21,38 +25,43 @@ class Quantum():
 
   def simulate(graph, start, simulations, steps):
 
-    N = graph.vertex_count()
+    with open("../generations/old_simulator.txt", "w") as f:
 
-    coin0 = np.array([1, 0])  # |0>
-    coin1 = np.array([0, 1])  # |1>
+      N = graph.vertex_count()
 
-    C00 = np.outer(coin0, coin0)  # |0><0|
-    C01 = np.outer(coin0, coin1)  # |0><1|
-    C10 = np.outer(coin1, coin0)  # |1><0|
-    C11 = np.outer(coin1, coin1)  # |1><1|
+      coin0 = np.array([1, 0])  # |0>
+      coin1 = np.array([0, 1])  # |1>
 
-    C_hat = (C00 + C01 + C10 - C11)/np.sqrt(2.)
+      C00 = np.outer(coin0, coin0)  # |0><0|
+      C01 = np.outer(coin0, coin1)  # |0><1|
+      C10 = np.outer(coin1, coin0)  # |1><0|
+      C11 = np.outer(coin1, coin1)  # |1><1|
 
-    ShiftPlus = np.roll(np.eye(N), 1, axis=0)
-    ShiftMinus = np.roll(np.eye(N), -1, axis=0)
-    S_hat = np.kron(ShiftPlus, C00) + np.kron(ShiftMinus, C11)
+      C_hat = (C00 + C01 + C10 - C11)/np.sqrt(2.)
 
-    U = S_hat.dot(np.kron(np.eye(N), C_hat))
+      ShiftPlus = np.roll(np.eye(N), 1, axis=0)
+      ShiftMinus = np.roll(np.eye(N), -1, axis=0)
+      S_hat = np.kron(ShiftPlus, C00) + np.kron(ShiftMinus, C11)
 
-    for _ in progressbar.progressbar(range(simulations)):
+      U = S_hat.dot(np.kron(np.eye(N), C_hat))
 
-      pos = np.zeros(N)
-      pos[start] = 1
+      for _ in progressbar.progressbar(range(simulations)):
 
-      psi0 = np.kron(pos, (coin0 + coin1 * 1j) / np.sqrt(2.))
-      counts = np.array([Quantum.measure(N, psi0)])
-      for step_i in range(1, steps + 1):
-        psii = np.linalg.matrix_power(U, step_i).dot(psi0)
-        prob = Quantum.measure(N, psii)
+        pos = np.zeros(N)
+        pos[start] = 1
+        psi0 = np.kron(pos, (coin0 + coin1 * 1j) / np.sqrt(2.))
+        counts = np.array([Quantum.measure(N, psi0)])
 
-      # TODO options = graph.neighbours(pos)
-        counts = np.concatenate((counts, np.array([prob])), axis=0)
+        out = [psi0]
+        for step_i in range(1, steps + 1):
+          psii = np.linalg.matrix_power(U, step_i).dot(psi0)
+          out = np.concatenate((out, np.array([psii])), axis=0)
+          prob = Quantum.measure(N, psii)
 
+        # TODO options = graph.neighbours(pos)
+          counts = np.concatenate((counts, np.array([prob])), axis=0)
+
+      f.write(np.array2string(out))
     return counts
 
   def describe():
