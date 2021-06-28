2446dde1991e808ce5624e7176ff876ad53c554e
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-06-28 21:54:36 +0200

    add out simulator

--- src/simulators/quantum.py
+++ src/simulators/quantum.py
@@ -6,13 +6,17 @@ from tqdm import tqdm
 from simulators.simulator import Simulator
 
 
+np.set_printoptions(threshold=np.inf)
+np.set_printoptions(linewidth=np.inf)
+
+
 class Quantum(Simulator):
 
   def probability(probability_amplitudes):
     return (probability_amplitudes.real**2 + probability_amplitudes.imag**2).sum(axis=1)
 
   def coin_start_state(size):
-    return [1/np.sqrt(2), 1j/np.sqrt(2)] + [0]*(size-2)
+    return np.array([1/np.sqrt(2), 1j/np.sqrt(2)] + [0]*(size-2))
 
   def coin(size):
     return splinalg.dft(size) / np.sqrt(size)
@@ -32,6 +36,7 @@ class Quantum(Simulator):
       counts = np.zeros((1, N), dtype=float)
       counts[0, start] = 1
 
+      out = [currpos.flatten()]
       for _ in tqdm(range(steps), leave=False):
         nextpos = np.zeros((N, regularity), dtype=complex)
         for i in tqdm(range(N), leave=False):
@@ -43,11 +48,15 @@ class Quantum(Simulator):
                     index] += currpos[i].dot(np.squeeze(multiplicators))
 
         currpos = nextpos
+        out = np.concatenate((out, np.array([currpos.flatten()])), axis=0)
 
         probabilities = Quantum.probability(currpos)
 
         counts = counts = np.concatenate(
             (counts, np.array([probabilities])), axis=0)
+
+    with open("../generations/new_simulator.txt", "w") as f:
+      f.write(np.array2string(out))
 
     return counts
 
