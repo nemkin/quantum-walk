c2d32d1874de79196f69cdae6562911b18df661d
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2021-07-26 19:14:10 +0200

    add

--- src/simulators/coins/hadamard.py
+++ src/simulators/coins/hadamard.py
@@ -1,12 +1,20 @@
+from commands.maths.kronecker_power import kronecker_power
+from commands.maths.is_power_of_2 import is_power_of_2
 import numpy as np
 import scipy.linalg as splinalg
 from simulators.coins.coin import Coin
+import math
 
 
 class Hadamard(Coin):
 
   def start(self):
-    return np.array([0.5, 0.5j, 0.5, 0.5j] + [0 for _ in range(self.size-4)])
+    if not is_power_of_2(self.size):
+      raise "Nem 2 hatvány a Hadamard érme oldalak száma!"
+
+    exp = int(np.log2(self.size))
+    size_2 = np.array([1/np.sqrt(2), 1j/np.sqrt(2)])
+    return kronecker_power(size_2, exp)
 
   def step(self):
     return splinalg.hadamard(self.size, dtype=complex) / np.sqrt(self.size)
